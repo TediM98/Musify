c2fb52f010b25f2c03d69f36ba04b2bcc30042b6
Noam Green <Noamgr11@gmail.com>
2023-06-01 15:59:21 +0300

    Add a station List

--- frontend/src/cmps/app-footer.jsx
+++ frontend/src/cmps/app-footer.jsx
@@ -1,7 +1,7 @@
 export function AppFooter() {
-  return (
-    <footer className="app-footer">
-      <h1>coffeerights - 2023™</h1>
-    </footer>
-  )
+  // return (
+  // <footer className="app-footer">
+  //   <h1>coffeerights - 2023™</h1>
+  // </footer>
+  // )
 }
