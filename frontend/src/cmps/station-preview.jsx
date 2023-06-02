648131bcca3a0ff505527c9abfecec75a689c58d
Noam Green <Noamgr11@gmail.com>
2023-06-02 14:53:11 +0300

    side navigation functinality and disghn

--- frontend/src/cmps/station-preview.jsx
+++ frontend/src/cmps/station-preview.jsx
@@ -1,15 +1,17 @@
-
-
+import { Link } from "react-router-dom";
 
 export function StationPreview({ station }) {
+
   return (
-    <section className="station-card">
-      <img src={station.createdBy.imgUrl} alt="" />
-      <h3>{station.name}</h3>
-      <div className="content">
-        <div>{station.songs[0].title}</div>
-        <div>{station.songs[1].title}</div>
-      </div>
-    </section>
+    <Link to={`/station/${station._id}`}>
+      <section className="station-card">
+        <img src={station.createdBy.imgUrl} alt="" />
+        <h3>{station.name}</h3>
+        <div className="content">
+          <div>{station.songs[0].title}</div>
+          <div>{station.songs[1].title}</div>
+        </div>
+      </section>
+    </Link>
   )
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -11,7 +11,6 @@ const routes = [
         path: '/',
         component: <StationIndex />,
         label: 'Home',
-
     },
     {
         path: 'search',
