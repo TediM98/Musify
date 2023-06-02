c4ea0a97d9c1002d2e9de55781e4c50f53702090
Omer Y <omeryossef@gmail.com>
2023-06-02 19:22:59 +0300

    implemented a basic player

--- frontend/src/cmps/player.jsx
+++ frontend/src/cmps/player.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react'
 import YouTube from 'react-youtube'
-import { Howl, Howler } from 'howler'
+
 import { trackService } from '../services/track.service'
 import { playerService } from '../services/player.service'
-import { storageService } from '../services/async-storage.service'
+
 
 //GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=The%20office&key=[YOUR_API_KEY] HTTP/1.1
 
-//console.log(trackService.getVideos('joy division')) ----------------DO NOT ERASE, COMMENTED TO PREVENT YT API BLOCK
+//console.log(trackService.getVideos('joy division')) //----------------DO NOT ERASE, COMMENTED TO PREVENT YT API BLOCK
 
 export function StationPlayer() {
   const [searchTerm, setSearchTerm] = useState(null)
+  const [player, setPlayer] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     playerService.getTop5Vids(searchTerm).then((res) => console.log('res', res))
@@ -21,15 +23,130 @@ export function StationPlayer() {
 
   let gTop5Vids = []
 
+  const handlePlay = () => {
+    if (player) {
+      if (!isPlaying) {
+        player.playVideo();
+        setIsPlaying(true);
+      } else {
+        player.pauseVideo();
+        setIsPlaying(false);
+      }
+    }
+  }
+
+  const handleStop = () => {
+    if (player) {
+      player.stopVideo();
+      setIsPlaying(false);
+    }
+  }
+
+  const handleForward = () => {
+    if (player) {
+      const currentTime = player.getCurrentTime();
+      player.seekTo(currentTime + 10, true);
+    }
+  }
+  const handleBackward = () => {
+    if (player) {
+      const currentTime = player.getCurrentTime();
+      player.seekTo(currentTime - 10, true);
+    }
+  }
+
+  const handlePlayerReady = (event) => {
+    setPlayer(event.target);
+  };
+
+  const opts = {
+    height: '0',
+    width: '0',
+    playerVars: {
+      autoplay: 0,
+      controls: 0,
+    },
+  };
+
+
   return (
-    <section className="player-container">
-      <div>
-        <iframe
-          width="420"
-          height="315"
-          src="https://www.youtube.com/embed/2EwViQxSJJQ"
-        ></iframe>
+    <div>
+      <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={handlePlayerReady}  /> 
+   <div>
+        <button onClick={handlePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleBackward}>Backward</button>
+        <button onClick={handleForward}>Forward</button>
       </div>
-    </section>
+    </div>
+
   )
 }
+
+// import React, { useState } from 'react';
+// import YouTube from 'react-youtube';
+
+// export function StationPlayer() {
+//   const [player, setPlayer] = useState(null);
+//   const [isPlaying, setIsPlaying] = useState(false);
+
+//   const handlePlay = () => {
+//     if (player) {
+//       if (!isPlaying) {
+//         player.playVideo();
+//         setIsPlaying(true);
+//       } else {
+//         player.pauseVideo();
+//         setIsPlaying(false);
+//       }
+//     }
+//   };
+
+//   const handleStop = () => {
+//     if (player) {
+//       player.stopVideo();
+//       setIsPlaying(false);
+//     }
+//   };
+
+//   const handleForward = () => {
+//     if (player) {
+//       const currentTime = player.getCurrentTime();
+//       player.seekTo(currentTime + 10, true);
+//     }
+//   };
+
+//   const handleBackward = () => {
+//     if (player) {
+//       const currentTime = player.getCurrentTime();
+//       player.seekTo(currentTime - 10, true);
+//     }
+//   };
+
+//   const handlePlayerReady = (event) => {
+//     setPlayer(event.target);
+//   };
+
+//   const opts = {
+//     height: '0',
+//       width: '0',
+//     playerVars: {
+//       autoplay: 0,
+//       controls: 0,
+//     },
+//   };
+
+//   return (
+//     <div>
+//       <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={handlePlayerReady} />
+
+//       <div>
+//         <button onClick={handlePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+//         <button onClick={handleStop}>Stop</button>
+//         <button onClick={handleBackward}>Backward</button>
+//         <button onClick={handleForward}>Forward</button>
+//       </div>
+//     </div>
+//   );
+// }
+
