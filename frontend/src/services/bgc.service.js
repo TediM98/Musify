03d5797425fd346e4b38fe7e37e50a8595565b15
Tedi Marunchak <tedimarunchack@gmail.com>
2023-06-02 19:23:37 +0300

    getBgc function changed in details cmp

--- frontend/src/services/bgc.service.js
+++ frontend/src/services/bgc.service.js
@@ -7,11 +7,24 @@ export const bgcService = {
 
 export async function getColorFromUrl(url) {
   try {
-    const fac = new FastAverageColor()
-    const color = await fac.getColorAsync(url)
-    return color.hex
-  }
-  catch {
-    console.log('Could not load bgc')
+    const fac = new FastAverageColor();
+    const color = await fac.getColorAsync(url);
+    console.log('color', color);
+    return color;
+  } catch (error) {
+    console.log('Could not load bgc:', error);
+    throw error; // Rethrow the error to propagate it to the caller if needed
   }
 }
+
+// export async function getColorFromUrl(url) {
+//   try {
+//     const fac = new FastAverageColor()
+//     const color = await fac.getColorAsync(url)
+//     console.log('color', color)
+//     return color
+//   }
+//   catch (err) {
+//     console.log('Could not load bgc:', err);
+//   }
+// }
