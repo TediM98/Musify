d030e27c034f284c9c1120d39a6d363034f2171d
Omer Y <omeryossef@gmail.com>
2023-06-03 11:29:07 +0300

    player UI, play and back/fwd buttons

--- frontend/src/cmps/player.jsx
+++ frontend/src/cmps/player.jsx
@@ -7,7 +7,7 @@ import { playerService } from '../services/player.service'
 import { setIsPlaying } from '../store/player.actions'
 //GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=The%20office&key=[YOUR_API_KEY] HTTP/1.1
 
-//console.log(trackService.getVideos('joy division')) //----------------DO NOT ERASE, COMMENTED TO PREVENT YT API BLOCK
+console.log(trackService.getVideos('joy division')) //----------------DO NOT ERASE, COMMENTED TO PREVENT YT API BLOCK
 
 export function StationPlayer() {
   const [searchTerm, setSearchTerm] = useState(null)
@@ -70,13 +70,13 @@ export function StationPlayer() {
   }
 
   return (
-    <div>
+    <div className='player-container'>
       <YouTube videoId="3tD2HJ-TQaM" opts={opts} onReady={handlePlayerReady} />
-      <div>
-        <button onClick={handlePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-        <button onClick={handleStop}>Stop</button>
-        <button onClick={handleBackward}>Backward</button>
-        <button onClick={handleForward}>Forward</button>
+      <div className='player'>
+        {/* <button className='stopBtn' onClick={handleStop}>Stop</button> */}
+        <button className='backBtn' onClick={handleBackward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z" fill="#b3b3b3"/></svg></button>
+        <button className='playBtn' onClick={handlePlay}>{isPlaying ? <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"/></svg>}</button>
+        <button className='fwdBton' onClick={handleForward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z" fill="#b3b3b3"/></svg></button>
       </div>
     </div>
   )
