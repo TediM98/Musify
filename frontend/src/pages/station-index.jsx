4434b2ff8532c3d967dbedd74391b81d5c39ba03
Noam Green <Noamgr11@gmail.com>
2023-06-04 00:38:33 +0300

    style app header

--- frontend/src/pages/station-index.jsx
+++ frontend/src/pages/station-index.jsx
@@ -6,8 +6,8 @@ import { loadStations, addStation, updateStation, removeStation, } from '../stor
 
 // For local service
 import { stationService } from '../services/station.service.local.js'
-
 import { StationList } from '../cmps/station-list.jsx'
+import { StationTalbe } from '../cmps/station-table.jsx'
 
 export function StationIndex() {
   const stations = useSelector(
@@ -18,32 +18,39 @@ export function StationIndex() {
     loadStations()
   }, [])
 
-  // async function onRemoveStation(stationId) {
-  //   try {
-  //     await removeStation(stationId)
-  //   } catch (err) {
-  //     console.log('Could not remove station')
-  //   }
-  // }
+  function getGreetings() {
+    var myDate = new Date();
+    var hrs = myDate.getHours();
+    console.log(hrs)
+    let greet;
 
-  // async function onAddStation() {
-  //   const station = stationService.getEmptyStation()
-  //   station.name = prompt('Vendor?')
-  //   try {
-  //     const savedStation = await addStation(station)
-  //   } catch (err) {
-  //     console.log('Cannot add station')
-  //   }
-  // }
+    if (hrs < 12) {
+      greet = "Good Morning";
+    } else if (hrs >= 12 && hrs <= 17) {
+      greet = "Good Afternoon";
+    } else if (hrs >= 17 && hrs <= 24) {
+      greet = "Good Evening";
+    }
+    return greet;
+  }
 
+  async function onRemoveStation(stationId) {
+    try {
+      await removeStation(stationId)
+    } catch (err) {
+      console.log('Could not remove station')
+    }
+  }
   // function onAddStationMsg(station) {
   //   console.log(`TODO Adding msg to station`)
   // }
-
   if (!stations) return <div>Loading...</div>
   return (
     <section className="main-layout home-page">
-      <h3>Good afterNoon</h3>
+      <section className='station-table main-layout'>
+        <h3>{getGreetings()}</h3>
+        <StationTalbe stations={stations} />
+      </section>
 
       <h3>Your top mixes</h3>
       <StationList stations={stations} />
@@ -51,7 +58,6 @@ export function StationIndex() {
       <StationList stations={stations} />
       <h3>Recently played</h3>
       <StationList stations={stations} />
-
     </section>
   )
 }
