44c85692f4d6f691b7110b04a14faed5c19b92e8
Omer Y <omeryossef@gmail.com>
2023-06-04 00:46:06 +0300

    basis for player store, removed bad query

--- frontend/src/store/player.reducer.js
+++ frontend/src/store/player.reducer.js
@@ -1,7 +1,8 @@
 export const SET_IS_PLAYING = 'SET_IS_PLAYING'
-
+export const SONG_IS_PLAYING = 'SONG_IS_PLAYING'
 const initialState = {
     isPlaying: false,
+    songPlaying: {}
 }
 
 export function playerReducer(state = initialState, action) {
@@ -9,6 +10,8 @@ export function playerReducer(state = initialState, action) {
     switch (action.type) {
         case SET_IS_PLAYING:
             return { ...state, isPlaying: action.isPlaying }
+        case SONG_IS_PLAYING:
+            return { ...state, songPlaying: action.songPlaying }
         default:
             return state;
     }
