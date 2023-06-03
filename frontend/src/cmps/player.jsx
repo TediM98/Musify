18aa7a2df5374c7d1f4b68eeb32429750af36d55
Omer Y <omeryossef@gmail.com>
2023-06-03 22:37:13 +0300

    updated API Key, continued improving the player

--- frontend/src/cmps/player.jsx
+++ frontend/src/cmps/player.jsx
@@ -78,81 +78,33 @@ export function StationPlayer() {
   }
 
   return (
-    <div className='player-container main-layout full'>
+    <div className='main-player-section full'>
+    <div className='player-container flex'>
       <YouTube videoId="3tD2HJ-TQaM" opts={opts} onReady={handlePlayerReady} />
-      <div className='player'>
-        {/* <button className='stopBtn' onClick={handleStop}>Stop</button> */}
-        <button className='backBtn' onClick={handleBackward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z" fill="#b3b3b3"/></svg></button>
-        <button className='playBtn' onClick={handlePlay}>{isPlaying ? <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"/></svg>}</button>
-        <button className='fwdBton' onClick={handleForward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z" fill="#b3b3b3"/></svg></button>
-      </div>
+      <div className='left-controls'>
+          left elements
+        </div>
+        <div className='center-controls'>
+
+          <div className='top-center-controls'>
+        <button className='backBtn' onClick={handleBackward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z" fill="#b3b3b3" /></svg></button>
+        <button className='playBtn' onClick={handlePlay}>{isPlaying ? <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z" /></svg>}</button>
+        <button className='fwdBton' onClick={handleForward}><svg xmlns="http://www.w3.org/2000/svg" role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 ldgdZj"><path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z" fill="#b3b3b3" /></svg></button>
+        </div>
+        
+        <div className='bottom-center-controls flex'>
+          <div className='time-stamp start'>00:00</div>
+        <div className='progress-bar '>
+          <input className="progress-bar-element" type='range' min='0' max='100'></input>
+        </div>
+        <div className='time-stamp end'>05:00</div>
+        </div>
+        </div>
+        <div className='right-controls'>
+          right elements
+        </div>
+      
+    </div>
     </div>
   )
 }
-
-// import React, { useState } from 'react';
-// import YouTube from 'react-youtube';
-
-// export function StationPlayer() {
-//   const [player, setPlayer] = useState(null);
-//   const [isPlaying, setIsPlaying] = useState(false);
-
-//   const handlePlay = () => {
-//     if (player) {
-//       if (!isPlaying) {
-//         player.playVideo();
-//         setIsPlaying(true);
-//       } else {
-//         player.pauseVideo();
-//         setIsPlaying(false);
-//       }
-//     }
-//   };
-
-//   const handleStop = () => {
-//     if (player) {
-//       player.stopVideo();
-//       setIsPlaying(false);
-//     }
-//   };
-
-//   const handleForward = () => {
-//     if (player) {
-//       const currentTime = player.getCurrentTime();
-//       player.seekTo(currentTime + 10, true);
-//     }
-//   };
-
-//   const handleBackward = () => {
-//     if (player) {
-//       const currentTime = player.getCurrentTime();
-//       player.seekTo(currentTime - 10, true);
-//     }
-//   };
-
-//   const handlePlayerReady = (event) => {
-//     setPlayer(event.target);
-//   };
-
-//   const opts = {
-//     height: '0',
-//       width: '0',
-//     playerVars: {
-//       autoplay: 0,
-//       controls: 0,
-//     },
-//   };
-
-//   return (
-//     <div>
-//       <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={handlePlayerReady} />
-
-//       <div>
-//         <button onClick={handlePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-//         <button onClick={handleStop}>Stop</button>
-//         <button onClick={handleBackward}>Backward</button>
-//         <button onClick={handleForward}>Forward</button>
-//       </div>
-//     </div>
-//   );
-// }
--- frontend/src/services/track.service.js
+++ frontend/src/services/track.service.js
@@ -1,16 +1,19 @@
 import { stationService } from "./station.service.local";
 import {utilService} from './util.service.js'
 import { httpService } from "./http.service.js";
+import axios from "axios";
 
 
 export const trackService = {
     getVideos 
 }
 const KEY = 'videosDB'
+const apiKey = 'AIzaSyB7HGhEzWj5biXGaa4zaAeKyGvcjzoiH3g'
+
 
 const debouncedGetVideos = utilService.debounce(getVideos);
-debouncedGetVideos('term') ////debounced getvideos
-
+// console.log(debouncedGetVideos('roxy music')) ////debounced getvideos
+console.log(getVideos('roxy music'))
 
 function getVideos(term) {
     const termVideosMap = utilService.loadFromStorage(KEY) || {}
@@ -18,7 +21,7 @@ function getVideos(term) {
 
     console.log('Getting from Network')
     
-    return httpService.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&maxResults=1&key=AIzaSyCp8KMTEjR9frWUGpSnc8Cw5cLVe7wRRDM&q=${term}`)
+    return httpService.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${term}&key=${apiKey}`)
         .then(res => res.data.items)
         .then(ytVideos => ytVideos.map(ytVideo => ({
             id: ytVideo.id.videoId,
@@ -36,4 +39,8 @@ function getVideos(term) {
         })
         
 }
+// const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${encodeURIComponent(term)}&key=${apiKey}`;
 
+// AIzaSyB7HGhEzWj5biXGaa4zaAeKyGvcjzoiH3g
+
+// OG STRING GET:   https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&maxResults=1&key=AIzaSyCp8KMTEjR9frWUGpSnc8Cw5cLVe7wRRDM&q=${term}
