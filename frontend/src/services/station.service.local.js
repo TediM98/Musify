dd1607a7bc8227c1bb8f17038c8d0326906f1530
Omer Y <omeryossef@gmail.com>
2023-06-10 22:16:09 +0300

    added: edit modal description editing, station details duration

--- frontend/src/services/station.service.local.js
+++ frontend/src/services/station.service.local.js
@@ -63,6 +63,7 @@ function getEmptyStation() {
     return {
         _id: "",
         name: "My playlist #1 ",
+        description:'',
         tags: [],
         createdBy: {
             _id: utilService.makeId(),
@@ -94,6 +95,7 @@ const demoStation =
         {
             "_id": "5cksxjas89xjsa8xjsa8jxs09",
             "name": "Relaxing Reading",
+            "description":"funky music",
             "tags": [
                 "Funk",
                 "Happy"
@@ -112,13 +114,15 @@ const demoStation =
                     "imgUrl": "https://i.ytimg.com/vi/4_iC0MyIykM/mqdefault.jpg",
                     "addedBy": '{minimal-user}',
                     "addedAt": 162521765262,
+                    "duration" : "03:01"
                 },
                 {
                     "_id": "mUkfiLjooxs",
                     "title": "The JB's - Pass The Peas",
                     "url": "https://www.youtube.com/watch?v=mUkfiLjooxs",
                     "imgUrl": "https://i.ytimg.com/vi/mUkfiLjooxs/mqdefault.jpg",
-                    "addedBy": {}
+                    "addedBy": {},
+                    "duration" : "03:01"
                 },
             ],
             "msgs": [
@@ -149,7 +153,8 @@ const demoStation =
                     "url": "https://www.youtube.com/watch?v=l482T0yNkeo",
                     "imgUrl": "https://i.ytimg.com/vi/l482T0yNkeo/mqdefault.jpg",
                     "addedBy": '{minimal-user}',
-                    "addedAt": 162521765263
+                    "addedAt": 162521765263,
+                    "duration" : "03:28"
                 },
                 {
                     "_id": "1w7OgIMMRc4",
@@ -157,7 +162,8 @@ const demoStation =
                     "url": "https://www.youtube.com/watch?v=1w7OgIMMRc4",
                     "imgUrl": "https://i.ytimg.com/vi/1w7OgIMMRc4/mqdefault.jpg",
                     "addedBy": '{minimal-user}',
-                    "addedAt": 162521765264
+                    "addedAt": 162521765264,
+                    "duration" : "05:03"
                 }
             ]
         },
@@ -181,7 +187,8 @@ const demoStation =
                     "url": "https://www.youtube.com/watch?v=tO4dxvguQDk",
                     "imgUrl": "https://i.ytimg.com/vi/tO4dxvguQDk/mqdefault.jpg",
                     "addedBy": '{minimal-user}',
-                    "addedAt": 162521765265
+                    "addedAt": 162521765265,
+                    "duration" : "03:07"
                 },
                 {
                     "_id": "jfKfPfyJRdk",
@@ -189,7 +196,8 @@ const demoStation =
                     "url": "https://www.youtube.com/watch?v=jfKfPfyJRdk",
                     "imgUrl": "https://i.ytimg.com/vi/5qap5aO4i9A/mqdefault.jpg",
                     "addedBy": '{minimal-user}',
-                    "addedAt": 162521765266
+                    "addedAt": 162521765266,
+                    "duration" : "53:07"
                 }
             ]
         },
