03891fc0c376e8bae22a84c4536edeb521c8069c
Tedi Marunchak <tedimarunchack@gmail.com>
2023-06-13 00:47:06 +0300

    genre-list component added

---
+++ frontend/src/cmps/genre-list.jsx
@@ -0,0 +1,135 @@
+export function GenreList() {
+  return (
+    <div>
+      <h1>Browse all</h1>
+      <div className="genres-cards-container">
+        <div
+          style={{ backgroundColor: '#e61e32' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Rock</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#148a08' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Pop</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#1e3264' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Relaxing</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#148a08' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Hip-Hop</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#d84000' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Electronic</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#1e3264' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Jazz</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#5179a1' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">R&B</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#e61e32' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Alternative</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#d84000 ' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Dance</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#bc5900' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Oriental</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#148a08' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Classical</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#e1118c' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Latin</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#bc9900' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Country</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#ec5502' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Chill</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#e1118c' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Mood</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#1e3264' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Sleep</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#e61e32' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Metal</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+        <div
+          style={{ backgroundColor: '#bc5900' }}
+          className="search-card-category"
+        >
+          <h1 className="search-card-headline">Jazz</h1>
+          <img className="search-card-img" src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/genres.jsx
+++ frontend/src/pages/genres.jsx
@@ -1,148 +1,37 @@
 import { useSelector } from 'react-redux'
 import { StationPreview } from '../cmps/station-preview'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { loadStations } from '../store/station.actions'
 
 export function GenresCards({}) {
   const stations = useSelector(
     (storeState) => storeState.stationModule.stations
   )
+  const [currGenreStations, setCurrGenreStations] = useState(null)
   const { stationGenre } = useParams()
 
-  useEffect(() => {}, [stationGenre])
+  useEffect(() => {
+    loadGenreStations()
+  }, [stationGenre])
+
+  async function loadGenreStations() {
+    try {
+      const stations = await loadStations()
+      const genreStations = stations.filter((station) =>
+        station.tags.includes(stationGenre)
+      )
+      setCurrGenreStations(genreStations)
+    } catch (err) {
+      console.log('Could not load Genres', err)
+    }
+  }
 
   function onCardClick() {}
 
   return (
-    <div>
-      <h1>Browse all</h1>
-      <div className="genres-cards-container">
-        <div
-          style={{ backgroundColor: '#e61e32' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Rock</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#148a08' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Pop</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#1e3264' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Relaxing</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#148a08' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Hip-Hop</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#d84000' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Electronic</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#1e3264' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Jazz</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#5179a1' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">R&B</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#e61e32' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Alternative</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#d84000 ' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Dance</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#bc5900' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Oriental</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#148a08' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Classical</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#e1118c' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Latin</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#bc9900' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Country</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#ec5502' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Chill</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#e1118c' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Mood</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#1e3264' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Sleep</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#e61e32' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Metal</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-        <div
-          style={{ backgroundColor: '#bc5900' }}
-          className="search-card-category"
-        >
-          <h1 className="search-card-headline">Jazz</h1>
-          <img className="search-card-img" src="" alt="" />
-        </div>
-      </div>
-    </div>
+    <section className="genre-stations">
+      <div className="">hello</div>
+    </section>
   )
 }
