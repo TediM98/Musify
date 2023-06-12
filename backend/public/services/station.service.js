a64fbf413a46642fde073757a20e76a56fe90c63
Omer Y <omeryossef@gmail.com>
2023-06-12 13:35:24 +0300

    edit and search page functionality and UI  enhancments abd fixes

--- backend/public/services/station.service.js
+++ backend/public/services/station.service.js
@@ -38,8 +38,15 @@ async function save(station) {
 
 function getEmptyStation() {
     return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
+        name: "My playlist #1 ",
+        description: '',
+        tags: [],
+        createdBy: {
+            owner:'tedi',
+            imgUrl: emptyStationImg
+        },
+        likedByUsers: [],
+        songs: [],
     }
 }
 
